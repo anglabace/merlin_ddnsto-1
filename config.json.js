a7d5ce09f0aa3f0b037db4d20f3aec7135ce4181
clangcn <clangcn@gmail.com>
2017-07-01 20:06:04 +0800

    ddnsto 1.3

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "koolshare小宝开发的基于http2的快速穿透。", 
     "home_url": "Module_ddnsto.asp", 
-    "md5": "dd24fde05dfdfa6ef2b93ee1f1101766", 
+    "md5": "349b5765f01dd59876363da315722e36", 
     "module": "ddnsto", 
     "title": "DDNSTO内网穿透", 
-    "version": "1.2"
+    "version": "1.3"
 }
